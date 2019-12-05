b8d26d2408940ad08aaf3e44d89979a2ae62a71f
Mateusz Pietrzak <57403837+mzpkjs@users.noreply.github.com>
2019-12-05 17:06:29 +0100

    feat(function): added curry function

--- packages/type/Function/Function.ts
+++ packages/type/Function/Function.ts
@@ -1,4 +1,5 @@
 import BinaryFunction from "./BinaryFunction/BinaryFunction"
+import CurriedFunction from "./CurriedFunction/CurriedFunction"
 import NullaryFunction from './NullaryFunction/NullaryFunction'
 import PartialFunction from "./PartialFunction/PartialFunction"
 import QuaternaryFunction from "./QuaternaryFunction/QuaternaryFunction"
@@ -41,8 +42,8 @@ namespace Function {
     export type Partial<TResult, TParameters extends unknown[], TApplied extends unknown[]> =
         PartialFunction<TResult, TParameters, TApplied>
 
-    // export type Curried<TResult, TParameters> =
-    //     CurriedFunction<TResult, TParameters>
+    export type Curried<TResult, TParameters> =
+        CurriedFunction<TResult, TParameters>
 }
 
 
