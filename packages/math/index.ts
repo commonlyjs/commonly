38ba66c7aa159ce169f2f3d9eac2ba2ded10281f
Mateusz Pietrzak <57403837+mzpkjs@users.noreply.github.com>
2019-12-06 14:02:44 +0100

    chore(docs): updated the documentation

--- packages/math/index.ts
+++ packages/math/index.ts
@@ -1,13 +1,21 @@
+import add from './add/add';
 import decrement from './decrement/decrement';
+import divide from './divide/divide';
 import increment from './increment/increment';
 import maximum from './maxiumum/maximum';
 import minimum from './minimum/minimum';
+import multiply from './multiply/multiply';
+import subtract from './subtract/subtract';
 
 
 
 export {
+    add,
     decrement,
+    divide,
     increment,
     maximum,
-    minimum
+    minimum,
+    multiply,
+    subtract
 }
