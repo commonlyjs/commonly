6481e67430200f39e829994a2ed341cc2957dbc0
Mateusz Pietrzak <57403837+mzpkjs@users.noreply.github.com>
2020-03-09 16:10:35 +0100

    chore(transducer): update documentation

--- packages/transducer/index.ts
+++ packages/transducer/index.ts
@@ -1,11 +1,13 @@
-import xfilter from './xfilter/xfilter';
-import xmap from './xmap/xmap';
-import xslice from './xslice/xslice';
-import xtake from './xtake/xtake';
+import xfind from './xfind/xfind'
+import xfilter from './xfilter/xfilter'
+import xmap from './xmap/xmap'
+import xslice from './xslice/xslice'
+import xtake from './xtake/xtake'
 
 
 
 export {
+    xfind,
     xfilter,
     xmap,
     xslice,
