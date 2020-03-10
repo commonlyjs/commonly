edf1a7415d0cc56d50e93bb21d6cadbb4fd27a36
Mateusz Pietrzak <57403837+mzpkjs@users.noreply.github.com>
2020-03-10 12:47:02 +0100

    fix(transducer): fix `xchain` transducer trying to process reduced value

--- packages/transducer/xchain/xchain.ts
+++ packages/transducer/xchain/xchain.ts
@@ -1,4 +1,6 @@
+import isReduced from "../../reflect/isReduced/isReduced"
 import Mapper from "../../type/Mapper/Mapper"
+import Reduced from "../../type/Reduced/Reduced"
 import Reducer from "../../type/Reducer/Reducer"
 import Transducer from "../../type/Transducer/Transducer"
 
@@ -15,8 +17,13 @@ import Transducer from "../../type/Transducer/Transducer"
 const xchain = <TValueA, TValueB = TValueA>(mapper: Mapper<TValueA, TValueB[]>): Transducer<TValueA, TValueB> =>
     <TAccumulator>(reducer: Reducer.Completing<TAccumulator, TValueB>) => {
         const transduced = (accumulator: TAccumulator, value: TValueA) => {
-            for (const v of mapper(value)) {
-                reducer(accumulator, v)
+            for (const x of mapper(value)) {
+                const product = reducer(accumulator, x)
+                if (!isReduced(product)) {
+                    accumulator = product
+                } else {
+                    break
+                }
             }
 
             return accumulator
