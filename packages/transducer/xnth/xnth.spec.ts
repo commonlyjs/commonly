4952a93ce7e1fdcfb505b7166fe4fc9c64d520f4
Mateusz Pietrzak <57403837+mzpkjs@users.noreply.github.com>
2020-03-10 13:44:03 +0100

    chore(transducer): update `xnth` transducer specification

--- packages/transducer/xnth/xnth.spec.ts
+++ packages/transducer/xnth/xnth.spec.ts
@@ -6,7 +6,7 @@ import xnth from "./xnth"
 
 
 
-describe("function xnth(i, j)", () => {
+describe("function xnth(i)", () => {
     context("transducing context is an array reduction", () => {
         const iterable = [ "0", 1, "1", 2, "3", 5, "8", 13, "21", 34 ]
         const reducer = <TValue>(accumulator: TValue[], value: TValue): TValue[] => {
