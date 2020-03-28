057faa8be9733ba02b16d398534b057b27180c23
Mateusz Pietrzak <57403837+mzpkjs@users.noreply.github.com>
2020-03-28 19:51:32 +0100

    feat(iterable): add `find` function

---
+++ packages/iterable/find/find.ts
@@ -0,0 +1,31 @@
+import xfilter from "../../transducer/xfilter/xfilter"
+import Predicate from "../../type/Predicate/Predicate"
+import transduce from '../transduce/transduce'
+import scalar from "../../function/reducing/scalar/scalar"
+import xfind from "../../transducer/xfind/xfind"
+
+
+
+/**
+ * Creates a new iterable of the same type as the one given, with only values passing the given predicate function.
+ *
+ * @since 1.0.0
+ *
+ * @param predicate - is a predicate
+ * @param iterable - is an iterable to be iterated over
+ * @returns an iterable of the same type as the one given
+ *
+ * @example
+ * ```
+ * import { filter } from "@commonly/iterable"
+ *
+ * filter(x => x % 2, [ 0, 1, 1, 2, 3, 5, 8 ])      // -> [ 1, 1, 2, 3, 5 ]
+ * ```
+ */
+const find = <TAccumulator, TValue>(predicate: Predicate<TValue>, iterable: Iterable<TValue>) => {
+    return transduce(xfind(predicate), iterable, scalar)
+}
+
+
+
+export default find
