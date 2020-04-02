93da7765bbba6d408499fb203a3cdefd25589de0
Mateusz Pietrzak <57403837+mzpkjs@users.noreply.github.com>
2020-04-02 18:26:27 +0200

    fix(iterable): update `map` function

--- packages/iterable/map/map.spec.ts
+++ packages/iterable/map/map.spec.ts
@@ -4,61 +4,213 @@ import map from "./map"
 
 describe("function map(mapper, iterable)", () => {
     context("iterable is an array", () => {
+        const mapper = (value: number): number => {
+            return value * value
+        }
+
         context("iterable is empty", () => {
-            const iterable: (number | string)[] = []
+            const iterable: number[] = []
 
             it("should return an empty array", () => {
-                expect(map(String, iterable))
+                expect(map(mapper, iterable))
+                    .toEqual([])
+                expect(map(mapper)(iterable))
                     .toEqual([])
             })
         })
 
         context("iterable is not empty", () => {
-            const iterable: (number | string)[] = [ "0", 1, "1", 2, "3", 5, "8", 13, "21", 34 ]
+            const iterable = [ 0, 1, 1, 2, 3, 5, 8, 13 ]
 
-            it("should return a mapped array", () => {
-                expect(map(String, iterable))
-                    .toEqual([ "0", "1", "1", "2", "3", "5", "8", "13", "21", "34" ])
+            it("should return an array where each element is raised to the power of two", () => {
+                expect(map(mapper, iterable))
+                    .toEqual([ 0, 1, 1, 4, 9, 25, 64, 169 ])
+                expect(map(mapper)(iterable))
+                    .toEqual([ 0, 1, 1, 4, 9, 25, 64, 169 ])
             })
         })
     })
 
     context("iterable is a string", () => {
+        const mapper = (value: string): number => {
+            return Number(value) * Number(value)
+        }
+
         context("iterable is empty", () => {
             const iterable: string = ""
 
             it("should return an empty string", () => {
-                expect(map(String, iterable))
+                expect(map(mapper, iterable))
+                    .toEqual("")
+                expect(map(mapper)(iterable))
                     .toEqual("")
             })
         })
 
         context("iterable is not empty", () => {
-            const iterable: string = "0112358132134"
+            const iterable: string = "011235813"
 
-            it("should return a mapped string", () => {
-                expect(map(String, iterable))
-                    .toEqual("0112358132134")
+            it("should return a string where each character is raised to the power of two", () => {
+                expect(map(mapper, iterable))
+                    .toEqual("01149256419")
+                expect(map(mapper)(iterable))
+                    .toEqual("01149256419")
             })
         })
     })
 
-    context("iterable is a Set", () => {
-        context("iterable is empty", () => {
-            const iterable: Set<number | string> = new Set()
+    context("iterable is an instance of Set", () => {
+        const mapper = (value: number): number => {
+            return value * value
+        }
 
-            it("should return an empty Set", () => {
-                expect(map(String, iterable))
+        context("iterable is empty", () => {
+            const iterable: Set<number> = new Set()
+
+            it("should return an empty instance of Set", () => {
+                expect(map(mapper, iterable))
+                    .toEqual(new Set())
+                expect(map(mapper)(iterable))
                     .toEqual(new Set())
             })
         })
 
         context("iterable is not empty", () => {
-            const iterable: Set<number | string> = new Set([ "0", 1, "1", 2, "3", 5, "8", 13, "21", 34 ])
+            const iterable: Set<number> = new Set([ 0, 1, 1, 2, 3, 5, 8, 13 ])
 
-            it("should return a mapped Set", () => {
-                expect(map(String, iterable))
-                    .toEqual(new Set([ "0", "1", "1", "2", "3", "5", "8", "13", "21", "34" ]))
+            it("should return an instance of Set where each element is raised to the power of two", () => {
+                expect(map(mapper, iterable))
+                    .toEqual(new Set([ 0, 1, 1, 4, 9, 25, 64, 169 ]))
+                expect(map(mapper)(iterable))
+                    .toEqual(new Set([ 0, 1, 1, 4, 9, 25, 64, 169 ]))
+            })
+        })
+    })
+
+    context("iterable is an instance of Map", () => {
+        const mapper = ([ key, value ]: [ number, number ]): [ number, number ] => {
+            return [ key * key, value * value ]
+        }
+
+        context("iterable is empty instance of Map", () => {
+            const iterable: Map<number, number> = new Map()
+
+            it("should return an empty instance of Map", () => {
+                expect(map(mapper, iterable))
+                    .toEqual(new Map())
+                expect(map(mapper)(iterable))
+                    .toEqual(new Map())
+            })
+        })
+
+        context("iterable is not empty", () => {
+            const iterable: Map<number, number> = new Map([
+                [ 0, 0 ], [ 1, 1 ], [ 2, 1 ], [ 3, 2 ], [ 4, 3 ], [ 5, 5 ], [ 6, 8 ], [ 7, 13 ]
+            ])
+
+            it("should return an instance of Map where each element is raised to the power of two", () => {
+                expect(map(mapper, iterable))
+                    .toEqual(new Map([
+                        [ 0, 0 ], [ 1, 1 ], [ 4, 1 ], [ 9, 4 ], [ 16, 9 ], [ 25, 25 ], [ 36, 64 ], [ 49, 169 ]
+                    ]))
+                expect(map(mapper)(iterable).set(1, 1))
+                    .toEqual(new Map([
+                        [ 0, 0 ], [ 1, 1 ], [ 4, 1 ], [ 9, 4 ], [ 16, 9 ], [ 25, 25 ], [ 36, 64 ], [ 49, 169 ]
+                    ]))
+            })
+        })
+    })
+
+    context("iterable is a custom data structure", () => {
+        const mapper = (value: number): number => {
+            return value * value
+        }
+
+        class Matrioshka<TValue> {
+            private _matrioshka: Matrioshka<TValue> | null = null
+            private readonly _value: TValue | null = null
+
+            constructor(values: TValue[] = []) {
+                const [ value, ...remainingValues ] = values
+
+                if (value) {
+                    this._value = value
+                }
+
+                if (remainingValues.length > 0) {
+                    this._matrioshka = new Matrioshka<TValue>(remainingValues)
+                }
+            }
+
+            public put(value: TValue): void {
+                this._matrioshka = new Matrioshka<TValue>([ value ])
+            }
+
+            public next(): Matrioshka<TValue> | null {
+                return this._matrioshka
+            }
+
+            public *[Symbol.iterator](): Iterator<TValue> {
+                if (this._value) {
+                    yield this._value
+                }
+
+                if (this._matrioshka) {
+                    yield* this._matrioshka
+                }
+            }
+
+            private ["@@reducer"]() {
+                const state = {
+                    root: null as Matrioshka<TValue> | null
+                }
+
+                const transduced = (accumulator: Matrioshka<TValue>, value: TValue) => {
+                    accumulator.put(value)
+                    const matrioshka = accumulator.next()
+                    if (state.root === null) {
+                        state.root = matrioshka
+                    }
+                    return matrioshka
+                }
+
+                transduced.initialize = () => {
+                    return new Matrioshka()
+                }
+
+                transduced.complete = (accumulator: Matrioshka<TValue>): Matrioshka<TValue> => {
+                    const parent = state.root
+                    state.root = null
+                    if (parent) {
+                        return parent
+                    } else {
+                        return accumulator
+                    }
+                }
+
+                return transduced
+            }
+        }
+
+        context("iterable is empty", () => {
+            const iterable: Matrioshka<number> = new Matrioshka()
+
+            it("should return an empty custom data structure", () => {
+                expect(map(mapper, iterable))
+                    .toEqual(new Matrioshka())
+                expect(map(mapper)(iterable))
+                    .toEqual(new Matrioshka())
+            })
+        })
+
+        context("iterable is not empty", () => {
+            const iterable = new Matrioshka([ 0, 1, 1, 2, 3, 5, 8, 13 ])
+
+            it("should return a custom data structure where each element is raised to the power of two", () => {
+                expect(map(mapper, iterable))
+                    .toEqual(new Matrioshka([ 0, 1, 1, 4, 9, 25, 64, 169 ]))
+                expect(map(mapper)(iterable))
+                    .toEqual(new Matrioshka([ 0, 1, 1, 4, 9, 25, 64, 169 ]))
             })
         })
     })
--- packages/iterable/map/map.ts
+++ packages/iterable/map/map.ts
@@ -1,12 +1,13 @@
-import xmap from "../../transducer/xmap/xmap"
 import Mapper from "../../type/Mapper/Mapper"
+import Iterable from "../../type/Iterable/Iterable"
+import curry from "../../function/curry/curry"
 import transduce from "../transduce/transduce"
-import delegate from "../../function/delegate/delegate"
+import xmap from "../../transducer/xmap/xmap"
 
 
 
 /**
- * Creates a new iterable of the same type as the one given, where every value is altered by a result of a given mapper function.
+ * Applies a mapper function to each element in the iterable, producing an iterable of the same type.
  *
  * @since 1.0.0
  *
@@ -18,13 +19,41 @@ import delegate from "../../function/delegate/delegate"
  * ```
  * import { map } from "@commonly/iterable"
  *
- * map(x => x * x, [ 0, 1, 1, 2, 3, 5, 8 ])     // -> [ 0, 1, 1, 4, 9, 25, 64 ]
+ * map(x => x * x, [ 0, 1, 1, 2, 3, 5, 8 ])         // -> [ 0, 1, 1, 4, 9, 25, 64 ]
  * ```
  */
-const map = <TAccumulator, TValueA, TValueB>(mapper: Mapper<TValueA, TValueB>, iterable: Iterable<TValueA>): Iterable<TValueB> => {
+const map = <TValueA, TValueB>(mapper: Mapper<TValueA, TValueB>, iterable: Iterable<TValueA>): Iterable<TValueB> => {
     return transduce(xmap(mapper), iterable)
 }
 
 
 
-export default delegate(map, (mapper, iterable) => iterable)
+export default curry(map) as unknown as {
+    //- uncurried signatures
+    <TIterable extends Iterable<unknown>>(
+        mapper: Mapper<Iterable.ExtractValue<TIterable>, Iterable.ExtractValue<TIterable>>,
+        iterable: TIterable
+    ): TIterable
+
+    <TValueA, TValueB>(mapper: Mapper<TValueA, TValueB>, iterable: TValueA[]): TValueB[]
+    <TValueA, TValueB>(mapper: Mapper<TValueA, TValueB>, iterable: string): string
+    <TValueA, TValueB>(mapper: Mapper<TValueA, TValueB>, iterable: Set<TValueA>): Set<TValueB>
+    <TValueA extends unknown[], TValueB extends unknown[]>(
+        mapper: Mapper<TValueA, TValueB>,
+        iterable: Map<TValueA[0], TValueA[1]>
+    ): Map<TValueB[0], TValueB[1]>
+
+
+    //- curried signatures
+    <TValueA, TValueB>(mapper: Mapper<TValueA, TValueB>): {
+        <TIterable extends Iterable<unknown>>(iterable: TIterable): TIterable
+
+        (iterable: TValueA[]): TValueB[]
+        (iterable: string): string
+        (iterable: Set<TValueA>): Set<TValueB>
+        (iterable: Map<unknown, unknown>): Map<
+            TValueB extends unknown[] ? TValueB[0] : never,
+            TValueB extends unknown[] ? TValueB[1] : never
+        >
+    }
+}
