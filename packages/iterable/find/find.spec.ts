831b07f2eb39c966149e2454025240188f026c63
Mateusz Pietrzak <57403837+mzpkjs@users.noreply.github.com>
2020-04-04 16:10:25 +0200

    chore(iterable): update `find` function specification

--- packages/iterable/find/find.spec.ts
+++ packages/iterable/find/find.spec.ts
@@ -1,64 +1,167 @@
+import Matrioshka from "../__fixtures__/Matrioshka"
 import find from "./find"
 
 
 
 describe("function find(predicate, iterable)", () => {
     context("iterable is an array", () => {
+        const predicate = (value: number): boolean => {
+            return value > 2
+        }
+
         context("iterable is empty", () => {
+            const iterable: number[] = []
+
             it("should return an undefined", () => {
-                const iterable: (number | string)[] = []
-                
-                expect(find(x => !!(Number(x) % 2), iterable))
-                    .toEqual(undefined)
+                const expected: undefined = undefined
+
+                expect(find(predicate, iterable))
+                    .toEqual(expected)
+                expect(find(predicate)(iterable))
+                    .toEqual(expected)
             })
         })
 
         context("iterable is not empty", () => {
-            const iterable: (number | string)[] = [ "0", 1, "1", 2, "3", 5, "8", 13, "21", 34 ]
+            const iterable: number[] = [ 0, 1, 1, 2, 3, 5, 8, 13 ]
 
-            it("should return a single value", () => {
-                expect(find(x => !!(Number(x) % 2) , iterable))
-                    .toEqual(1)
+            it("should return a first found value which is greater than two", () => {
+                const expected: number = 3
+
+                expect(find(predicate, iterable))
+                    .toEqual(expected)
+                expect(find(predicate)(iterable))
+                    .toEqual(expected)
             })
         })
     })
 
     context("iterable is a string", () => {
-        context("iterable is empty", () => {
-            it("should return an undefined", () => {
-                const iterable: string = ""
+        const predicate = (value: string): boolean => {
+            return Number(value) > 2
+        }
 
-                expect(find(x => !!(Number(x) % 2), iterable))
-                    .toEqual(undefined)
+        context("iterable is empty", () => {
+            const iterable: string = ""
+
+            it("should return an undefined", () => {
+                const expected: undefined = undefined
+
+                expect(find(predicate, iterable))
+                    .toEqual(expected)
+                expect(find(predicate)(iterable))
+                    .toEqual(expected)
             })
         })
 
         context("iterable is not empty", () => {
             const iterable: string = "0112358132134"
 
-            it("should return a single value", () => {
-                expect(find(x => !!(Number(x) % 2) , iterable))
-                    .toEqual("1")
+            it("should return a first found value which is greater than two", () => {
+                const expected: string = "3"
+
+                expect(find(predicate, iterable))
+                    .toEqual(expected)
+                expect(find(predicate)(iterable))
+                    .toEqual(expected)
             })
         })
     })
 
-    context("iterable is a Set", () => {
-        context("iterable is empty", () => {
-            it("should return an undefined", () => {
-                const iterable: Set<number | string> = new Set()
+    context("iterable is an instance of Set", () => {
+        const predicate = (value: number): boolean => {
+            return value > 2
+        }
 
-                expect(find(x => !!(Number(x) % 2), iterable))
-                    .toEqual(undefined)
+        context("iterable is empty", () => {
+            const iterable: Set<number> = new Set()
+
+            it("should return an undefined", () => {
+                const expected: undefined = undefined
+
+                expect(find(predicate, iterable))
+                    .toEqual(expected)
+                expect(find(predicate)(iterable))
+                    .toEqual(expected)
             })
         })
 
         context("iterable is not empty", () => {
-            const iterable: Set<number | string> = new Set([ "0", 1, "1", 2, "3", 5, "8", 13, "21", 34 ])
+            const iterable: Set<number> = new Set([ 0, 1, 1, 2, 3, 5, 8, 13 ])
 
-            it("should return a single value", () => {
-                expect(find(x => !!(Number(x) % 2) , iterable))
-                    .toEqual(1)
+            it("should return a first found value which is greater than two", () => {
+                const expected: number = 3
+
+                expect(find(predicate, iterable))
+                    .toEqual(expected)
+                expect(find(predicate)(iterable))
+                    .toEqual(expected)
+            })
+        })
+    })
+
+    context("iterable is an instance of Map", () => {
+        const predicate = ([ key, value ]: [ number, number ]): boolean => {
+            return (key + value) > 2
+        }
+
+        context("iterable is empty", () => {
+            const iterable: Map<number, number> = new Map()
+
+            it("should return an undefined", () => {
+                const expected: undefined = undefined
+
+                expect(find(predicate, iterable))
+                    .toEqual(expected)
+                expect(find(predicate)(iterable))
+                    .toEqual(expected)
+            })
+        })
+
+        context("iterable is not empty", () => {
+            const iterable: Map<number, number> = new Map([
+                [ 0, 0 ], [ 1, 1 ], [ 2, 1 ], [ 3, 2 ], [ 4, 3 ], [ 5, 5 ], [ 6, 8 ], [ 7, 13 ]
+            ])
+
+            it("should return a first found element which sum of key and value is greater than two", () => {
+                const expected: [ number, number ] = [ 2, 1 ]
+
+                expect(find(predicate, iterable))
+                    .toEqual(expected)
+                expect(find(predicate)(iterable))
+                    .toEqual(expected)
+            })
+        })
+    })
+
+    context("iterable is a custom finite iterator-based collection", () => {
+        const predicate = (value: number): boolean => {
+            return value > 2
+        }
+
+        context("iterable is empty", () => {
+            const iterable: Matrioshka<number> = new Matrioshka()
+
+            it("should return an undefined", () => {
+                const expected: undefined = undefined
+
+                expect(find(predicate, iterable))
+                    .toEqual(expected)
+                expect(find(predicate)(iterable))
+                    .toEqual(expected)
+            })
+        })
+
+        context("iterable is not empty", () => {
+            const iterable: Matrioshka<number> = new Matrioshka([ 0, 1, 1, 2, 3, 5, 8, 13 ])
+
+            it("should return a first found value which is greater than two", () => {
+                const expected: number = 3
+
+                expect(find(predicate, iterable))
+                    .toEqual(expected)
+                expect(find(predicate)(iterable))
+                    .toEqual(expected)
             })
         })
     })
--- packages/iterable/find/find.ts
+++ packages/iterable/find/find.ts
@@ -1,26 +1,27 @@
-import xfilter from "../../transducer/xfilter/xfilter"
+import Iterable from "../../type/Iterable/Iterable"
 import Predicate from "../../type/Predicate/Predicate"
-import transduce from '../transduce/transduce'
-import xfind from "../../transducer/xfind/xfind"
+import curry from "../../function/curry/curry"
 import reducing from "../../function/reducing/reducing"
-import delegate from "../../function/delegate/delegate"
+import xfind from "../../transducer/xfind/xfind"
+import transduce from '../transduce/transduce'
 
 
 
 /**
- * Creates a new iterable of the same type as the one given, with only values passing the given predicate function.
+ * Applies a `predicate` function to each element in the `iterable`,
+ *  returning an element passing this predicate.
  *
  * @since 1.0.0
  *
- * @param predicate - is a predicate
+ * @param predicate - is a predicate function
  * @param iterable - is an iterable to be iterated over
  * @returns an iterable of the same type as the one given
  *
  * @example
  * ```
- * import { filter } from "@commonly/iterable"
+ * import { find } from "@commonly/iterable"
  *
- * filter(x => x % 2, [ 0, 1, 1, 2, 3, 5, 8 ])      // -> [ 1, 1, 2, 3, 5 ]
+ * find(x => x > 2, [ 0, 1, 1, 2, 3, 5, 8 ])      // -> 3
  * ```
  */
 const find = <TAccumulator, TValue>(predicate: Predicate<TValue>, iterable: Iterable<TValue>) => {
@@ -29,4 +30,26 @@ const find = <TAccumulator, TValue>(predicate: Predicate<TValue>, iterable: Iter
 
 
 
-export default delegate(find)
+export default curry(find) as unknown as {
+    <TValue>(predicate: Predicate<TValue>, iterable: TValue[]): TValue
+    <TValue>(predicate: Predicate<TValue>, iterable: string): string
+    <TValue>(predicate: Predicate<TValue>, iterable: Set<TValue>): TValue
+    <TValue extends unknown[]>(
+        predicate: Predicate<TValue>,
+        iterable: Map<TValue[0], TValue[1]>
+    ): TValue
+    <TIterable extends Iterable<unknown>>(
+        predicate: Predicate<Iterable.ExtractValue<TIterable>>,
+        iterable: TIterable
+    ): Iterable.ExtractValue<TIterable>
+
+    <TValue>(predicate: Predicate<TValue>): {
+        (iterable: TValue[]): TValue
+        (iterable: string): string
+        (iterable: Set<TValue>): TValue
+        (iterable: Map<unknown, unknown>): TValue
+        <TIterable extends Iterable<unknown>>(
+            iterable: TIterable
+        ): TValue
+    }
+}
