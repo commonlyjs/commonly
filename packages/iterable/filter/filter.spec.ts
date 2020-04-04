88b50d83fff6ba5c397fb139a5eb93d163b62850
Mateusz Pietrzak <57403837+mzpkjs@users.noreply.github.com>
2020-04-04 15:26:06 +0200

    chore(iterable): update `filter` function specification

--- packages/iterable/filter/filter.spec.ts
+++ packages/iterable/filter/filter.spec.ts
@@ -1,64 +1,169 @@
 import filter from "./filter"
+import Matrioshka from "../__fixtures__/Matrioshka"
 
 
 
 describe("function filter(predicate, iterable)", () => {
     context("iterable is an array", () => {
+        const predicate = (value: number): boolean => {
+            return Boolean(value % 2)
+        }
+
         context("iterable is empty", () => {
-            const iterable: (number | string)[] = []
+            const iterable: number[] = []
 
             it("should return an empty array", () => {
-                expect(filter(x => !!(Number(x) % 2), iterable))
-                    .toEqual([])
+                const expected: number[] = []
+
+                expect(filter(predicate, iterable))
+                    .toEqual(expected)
+                expect(filter(predicate)(iterable))
+                    .toEqual(expected)
             })
         })
 
         context("iterable is not empty", () => {
-            const iterable: (number | string)[] = [ "0", 1, "1", 2, "3", 5, "8", 13, "21", 34 ]
+            const iterable: number[] = [ 0, 1, 1, 2, 3, 5, 8, 13 ]
 
-            it("should return an array containing only odd values", () => {
-                expect(filter(x => !!(Number(x) % 2), iterable))
-                    .toEqual([ 1, "1", "3", 5, 13, "21" ])
+            it("should return an array containing only even values", () => {
+                const expected: number[] = [ 1, 1,3, 5, 13 ]
+
+                expect(filter(predicate, iterable))
+                    .toEqual(expected)
+                expect(filter(predicate)(iterable))
+                    .toEqual(expected)
             })
         })
     })
 
     context("iterable is a string", () => {
+        const predicate = (value: string): boolean => {
+            return Boolean(Number(value) % 2)
+        }
+
         context("iterable is empty", () => {
             const iterable: string = ""
 
             it("should return an empty string", () => {
-                expect(filter(x => !!(Number(x) % 2), iterable))
-                    .toEqual("")
+                const expected: string = ""
+
+                expect(filter(predicate, iterable))
+                    .toEqual(expected)
+                expect(filter(predicate)(iterable))
+                    .toEqual(expected)
             })
         })
 
         context("iterable is not empty", () => {
-            const iterable: string = "0112358132134"
+            const iterable: string = "011235813"
 
             it("should return a string containing only odd values", () => {
-                expect(filter(x => !!(Number(x) % 2), iterable))
-                    .toEqual("11351313")
+                const expected: string = "113513"
+
+                expect(filter(predicate, iterable))
+                    .toEqual(expected)
+                expect(filter(predicate)(iterable))
+                    .toEqual(expected)
             })
         })
     })
 
-    context("iterable is a Set", () => {
-        context("iterable is empty", () => {
-            const iterable: Set<void> = new Set()
+    context("iterable is an instance of Set", () => {
+        const predicate = (value: number): boolean => {
+            return Boolean(value % 2)
+        }
 
-            it("should return an empty Set", () => {
-                expect(filter(x => !!(Number(x) % 2), iterable))
-                    .toEqual(new Set())
+        context("iterable is empty", () => {
+            const iterable: Set<number> = new Set()
+
+            it("should return an empty instance of Set", () => {
+                const expected: Set<number> = new Set()
+
+                expect(filter(predicate, iterable))
+                    .toEqual(expected)
+                expect(filter(predicate)(iterable))
+                    .toEqual(expected)
             })
         })
 
         context("iterable is not empty", () => {
-            const iterable: Set<number | string> = new Set([ "0", 1, "1", 2, "3", 5, "8", 13, "21", 34 ])
+            const iterable: Set<number> = new Set([ 0, 1, 1, 2, 3, 5, 8, 13 ])
 
-            it("should return a Set containing only odd values", () => {
-                expect(filter(x => !!(Number(x) % 2), iterable))
-                    .toEqual(new Set([ 1, "1", "3", 5, 13, "21" ]))
+            it("should return an instance of Set containing only odd values", () => {
+                const expected: Set<number> = new Set([ 1, 3, 5, 13 ])
+
+                expect(filter(predicate, iterable))
+                    .toEqual(expected)
+                expect(filter(predicate)(iterable))
+                    .toEqual(expected)
+            })
+        })
+    })
+
+    context("iterable is an instance of Map", () => {
+        const predicate = ([ key, value ]: [ number, number ]): boolean => {
+            return Boolean((key + value) % 2)
+        }
+
+        context("iterable is empty", () => {
+            const iterable: Map<number, number> = new Map()
+
+            it("should return an empty instance of Map", () => {
+                const expected: Map<number, number> = new Map()
+
+                expect(filter(predicate, iterable))
+                    .toEqual(expected)
+                expect(filter(predicate)(iterable))
+                    .toEqual(expected)
+            })
+        })
+
+        context("iterable is not empty", () => {
+            const iterable: Map<number, number> = new Map([
+                [ 0, 0 ], [ 1, 1 ], [ 2, 1 ], [ 3, 2 ], [ 4, 3 ], [ 5, 5 ], [ 6, 8 ], [ 7, 13 ]
+            ])
+
+            it("should return an instance of Map containing only values of which sum of key and value is odd", () => {
+                const expected: Map<number, number> = new Map([
+                    [ 2, 1 ], [ 3, 2 ], [ 4, 3 ]
+                ])
+
+                expect(filter(predicate, iterable))
+                    .toEqual(expected)
+                expect(filter(predicate)(iterable))
+                    .toEqual(expected)
+            })
+        })
+    })
+
+    context("iterable is a custom finite iterator-based collection", () => {
+        const predicate = (value: number): boolean => {
+            return Boolean(value % 2)
+        }
+
+        context("iterable is empty", () => {
+            const iterable: Matrioshka<number> = new Matrioshka()
+
+            it("should return an empty custom collection", () => {
+                const expected: Matrioshka<number> = new Matrioshka()
+
+                expect(filter(predicate, iterable))
+                    .toEqual(expected)
+                expect(filter(predicate)(iterable))
+                    .toEqual(expected)
+            })
+        })
+
+        context("iterable is not empty", () => {
+            const iterable: Matrioshka<number> = new Matrioshka([ 0, 1, 1, 2, 3, 5, 8, 13 ])
+
+            it("should return a custom collection containing only odd values", () => {
+                const expected: Matrioshka<number> = new Matrioshka([ 1, 1, 3, 5, 13 ])
+
+                expect(filter(predicate, iterable))
+                    .toEqual(expected)
+                expect(filter(predicate)(iterable))
+                    .toEqual(expected)
             })
         })
     })
--- packages/iterable/filter/filter.ts
+++ packages/iterable/filter/filter.ts
@@ -1,16 +1,19 @@
-import xfilter from "../../transducer/xfilter/xfilter"
 import Predicate from "../../type/Predicate/Predicate"
+import curry from "../../function/curry/curry"
+import xfilter from "../../transducer/xfilter/xfilter"
 import transduce from '../transduce/transduce'
-import delegate from "../../function/delegate/delegate"
+import Mapper from "../../type/Mapper/Mapper"
+import Iterable from "../../type/Iterable/Iterable"
 
 
 
 /**
- * Creates a new iterable of the same type as the one given, with only values passing the given predicate function.
+ * Applies a `predicate` function to each element in the `iterable`,
+ *  producing an iterable of the same type with elements passing this predicate.
  *
  * @since 1.0.0
  *
- * @param predicate - is a predicate
+ * @param predicate - is a predicate function
  * @param iterable - is an iterable to be iterated over
  * @returns an iterable of the same type as the one given
  *
@@ -27,4 +30,27 @@ const filter = <TAccumulator, TValue>(predicate: Predicate<TValue>, iterable: It
 
 
 
-export default delegate(filter)
+export default curry(filter) as unknown as {
+    <TValue>(predicate: Predicate<TValue>, iterable: TValue[]): TValue[]
+    <TValue>(predicate: Predicate<TValue>, iterable: string): string
+    <TValue>(predicate: Predicate<TValue>, iterable: Set<TValue>): Set<TValue>
+    <TValue extends unknown[]>(
+        predicate: Predicate<TValue>,
+        iterable: Map<TValue[0], TValue[1]>
+    ): Map<TValue[0], TValue[1]>
+    <TIterable extends Iterable<unknown>>(
+        mapper: Predicate<Iterable.ExtractValue<TIterable>>,
+        iterable: TIterable
+    ): TIterable
+
+    <TValue>(mapper: Predicate<TValue>): {
+        (iterable: TValue[]): TValue[]
+        (iterable: string): string
+        (iterable: Set<TValue>): Set<TValue>
+        (iterable: Map<unknown, unknown>): Map<TValue extends unknown[] ? TValue[0] : never,
+            TValue extends unknown[] ? TValue[1] : never>
+        <TIterable extends Iterable<unknown>>(
+            iterable: TIterable
+        ): TIterable
+    }
+}
